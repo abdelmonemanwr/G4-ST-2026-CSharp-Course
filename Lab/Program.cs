5fa6603307be56b6884ff51d6029a904ec249b06
Men3m <75037589+abdelmonemanwr@users.noreply.github.com>
2026-08-13 12:50:47 +0300

    day4. lab structure demo

---
+++ Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region emp 
+            Employee emp = new Employee();
+            //emp.id
+            #endregion
+        }
+    }
+}
